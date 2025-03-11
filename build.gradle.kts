1da8aaaff0f98e6e9efb872d5ee7b7dd6cbb3032
Alexis Manin <alexis.manin@gmail.com>
2025-03-11 21:16:42 +0100

    Add JMH Html report task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.10"
     id("me.champeau.jmh") version "0.7.2"
+    id("io.morethan.jmhreport") version "0.9.6"
 }
 
 repositories {
@@ -26,6 +27,26 @@ tasks {
     }
 }
 
+val jmhResultFile = project.layout.buildDirectory.file("results/jmh/results.json").get()
+val jmhReportDir = project.layout.buildDirectory.dir("reports/jmh").get()
+
 jmh {
     resultFormat = "json"
+    resultsFile = jmhResultFile
+}
+
+jmhReport {
+    jmhResultPath = jmhResultFile.toString()
+    jmhReportOutput = jmhReportDir.toString()
+}
+
+val jmhExecutionTask = tasks.named("jmh")
+
+tasks.named("jmhReport") {
+    // Ensure JMH has been executed before producing the report, otherwise launch it
+    inputs.files(jmhExecutionTask.get().outputs.files)
+    // Workaround: Must explicitly create output folder, otherwise an error is raised
+    doFirst {
+        jmhReportDir.asFile.mkdirs()
+    }
 }
